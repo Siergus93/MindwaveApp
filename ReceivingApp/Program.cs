cdc757ebf680973cdd097d9e0fb4429980b9915b
Sektu <sirrotto@gmail.com>
2015-11-16 16:54:45 +0100

    Added code for receiving a data from PuzzleApp.

--- ReceivingApp/Program.cs
+++ ReceivingApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using libUCLA;
 
 namespace ReceivingApp
 {
@@ -9,6 +7,26 @@ namespace ReceivingApp
     {
         static void Main(string[] args)
         {
+            try
+            {
+                ULoader_JSON config = new ULoader_JSON(@"config.json";)
+                UReceiver uReceiver = config.GetReceiver("input1");
+                uReceiver.DataReceived += new UReceiveHandler(onData);
+                uReceiver.Receive();
+
+            }
+            catch(UException exc)
+            {
+                Console.Write(exc.Message);
+            }
+        }
+
+        private static void OnData(object sender, UDataReceivedArgs data)
+        {
+            foreach (byte b in data.Buffer)
+            {
+                Console.Write(b + "\t");
+            }
         }
     }
 }
